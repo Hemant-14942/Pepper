2a6df18bbb2c4358b3afc018254b769d89e474c1
Hemant-14942 <hemantinsan6@gmail.com>
2025-03-14 13:05:56 +0530

    Changes

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -4,14 +4,15 @@ import LabelledSection from '../components/LabelledSection'
 import HeroCard from '../components/HeroCard'
 import CardsSection from '../components/CardsSection'
 import AnimCards from '../components/AnimCards'
+import AnimCardsSec from '../components/AnimCardsSec'
+import  Dropdown  from '../components/Dropdown'
 const Home = () => {
   return (
     <div>
       <HeroSection/>
       <LabelledSection/>
       <CardsSection/>
-      {/* <HeroCard/> */}
-      {/* <AnimCards/> */}
+      <AnimCardsSec/>
     </div>
   )
 }
