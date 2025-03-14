573b7b583505a24f7bcf7548ba43ebd84897e707
Hemant-14942 <hemantinsan6@gmail.com>
2025-03-14 13:19:48 +0530

    Fixed Margins

--- Frontend/src/components/HeroCardSection.jsx
+++ Frontend/src/components/HeroCardSection.jsx
@@ -3,7 +3,7 @@ import HeroCard from "./HeroCard";
 
 const HeroCardSection = () => {
   return (
-    <div className="mt-20 px-10 w-full">
+    <div className="mt-20 px-32 w-full">
       <div className="text flex flex-col items-center gap-6">
           <h1 className="text-5xl font-bold">Hot Pizza, Hotter Deals</h1>
           <p className="text-2xl">
--- Frontend/src/components/LabelledSection.jsx
+++ Frontend/src/components/LabelledSection.jsx
@@ -5,11 +5,11 @@ const LabelledSection = ({data}) => {
   return (
 
     <div className='bg-light-pink flex flex-col py-10 space-y-10 -mt-44'>
-      <div className='text-center space-y-6 '>
+      <div className='text-center space-y-6  '>
         <div className='font-bold text-5xl text-gray'>Fan Favorites</div>
         <div className='text-xl text-gray'>From classic combinations to bold flavors, these pizzas top our list for a reason.</div>
       </div>
-      <div className='flex justify-center items-center gap-10 w-full px-10'>
+      <div className='flex justify-center items-center gap-10 w-full px-32'>
       {
         data?.map((item,index)=>{
           return <LabelCards image={item?.image}/>
